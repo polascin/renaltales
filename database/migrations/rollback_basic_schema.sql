ada9a545a01acbdfc04915aa02c40dda834eb48c
polascin <lubomir@polascin.net>
2025-07-05 12:01:49 +0200

    Remove unit tests for Story and User models to streamline testing framework

--- database/migrations/rollback_basic_schema.sql
+++
@@ -1,8 +0,0 @@
--- Rollback migration script for basic schema
--- Drops stories and users tables in correct order (foreign key dependencies)
-
--- Drop stories table first (has foreign key to users)
-DROP TABLE IF EXISTS stories;
-
--- Drop users table
-DROP TABLE IF EXISTS users;
