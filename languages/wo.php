de5c58001a7996c9191ee3db93303087312b78c6
polascin <lubomir@polascin.net>
2025-07-09 09:35:26 +0200

    fix: remove wo.php and add languages/wo.php

---
+++ languages/wo.php
@@ -0,0 +1,17 @@
+<?php
+return [
+  'app_title' => 'Leeral yi ñuy joxe Neeñ',
+  'welcome' => 'Dalal ak jamm',
+  'current_language' => 'Làkk bi ñu jëfandikoo tay',
+  'footer_copyright' => 'Ľubomír Polaščín',
+  'language_selection' => 'Tànnal làkk',
+  'language_change' => 'Soppi làkk',
+  'language_changed' => 'Làkk bi soppi na',
+  'language_not_found' => 'Fiise bu làkk bi amul',
+  'language_default' => 'Làkk bu njëkk',
+  'language_default_missing' => 'Fiise bu làkk bu njëkk bi amul',
+  'language_switch' => 'Soppi làkk',
+  'language_switch_success' => 'Làkk bi soppi na bu baax',
+  'language_switch_error' => 'Njumte ci soppeeku làkk bi'
+];
+?>
